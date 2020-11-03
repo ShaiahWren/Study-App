2cf5ba0eef8d9f656d404a8eb28d18dc98cc2b9b
Shaiah Wren <sde2111@columbia.edu>
2020-11-03 14:48:02 -0500

    built login page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -14,7 +14,7 @@ const LandingPage = () => {
                     <div className="nav-wrapper">
                         <a href="#" className="brand-logo center">Logo</a>
                         <ul id="nav-mobile" className="right">
-                            <li><a href="sass.html">Sign In</a></li>
+                            <li><a href="/signin">Sign In</a></li>
 
                         </ul>
                     </div>
@@ -32,8 +32,7 @@ const LandingPage = () => {
                         <img className="intro-images" src="http://placekitten.com/200/300" alt="" />
 
                         <h2>Flashcards</h2>
-                        <p>Quiz yourself on JS, React, etc!
-                </p>
+                        <p>Quiz yourself on JS, React, etc!</p>
                     </div>
                     <div className="card">
                         <img className="intro-images" src="http://placekitten.com/200/300" alt="" />
@@ -65,6 +64,32 @@ const LandingPage = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo vel orci porttitor malesuada. Proin sed tempor eros, sit amet dapibus turpis. Praesent vitae elit quis nibh facilisis eleifend. In id viverra tellus, sed varius mauris. Suspendisse vel ultrices urna. Sed sit amet dui et nisl dapibus feugiat sed eget mi. Mauris nunc velit, pulvinar id augue eget, vulputate dapibus sapien. Vestibulum est felis, ornare a purus vitae, scelerisque vestibulum diam. Fusce vehicula mi eget velit posuere, quis condimentum mi finibus. Nullam maximus ex nec ex posuere elementum. Cras at efficitur erat. Curabitur sed mauris purus. Aliquam feugiat mi eget purus dapibus, a vulputate nisl feugiat. Fusce semper eu odio sit amet posuere. Morbi quis velit nisl. Nunc eget ante sagittis dui finibus facilisis at vitae nunc.</p>
                     </div>
                 </div>
+
+                <footer className="page-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <h5 className="white-text">Footer Content</h5>
+                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div className="col l4 offset-l2 s12">
+                <h5 className="white-text">Links</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2014 Copyright Text
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
 
 
 
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
 
 const Signin = () => {
     return (
         <>
-            <h1>Signin</h1>
+            <Route path="/signin">
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#" className="brand-logo center">Logo</a>
+                        <ul id="nav-mobile" className="right">
+                            <li><a href="/">Home</a></li>
+
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="card-wrapper">
+                    <div className="sign-up-card">
+                        <h1>Signup</h1>
+                        <label >
+                            <input type="email" placeholder="cardib@hotmail.com" />
+                        </label>
+
+                        <label >
+                            <input type="password" placeholder="*************" />
+
+                        </label>
+                        <label >
+                            <button><Link to="/dashboard">
+                                Submit
+                            </Link></button>
+                        </label>
+
+
+                    </div>
+
+                    <p>or</p>
+
+                    <div className="sign-up-card">
+                        <h1>Signup</h1>
+                        <label >
+                            <input type="email" placeholder="cardib@hotmail.com" />
+                        </label>
+
+                        <label >
+                            <input type="password" placeholder="*************" />
+
+                        </label>
+                        <label >
+                            <button><Link to="/dashboard">
+                                Submit
+                            </Link></button>
+                        </label>
+
+                    </div>
+
+
+                </div>
+
+            </Route>
+
+
         </>
     )
 };
