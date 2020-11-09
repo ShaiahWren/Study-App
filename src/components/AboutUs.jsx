f8825fdc50a6c8434cb58af34635c9a67a4057d5
Shaiah Wren <sde2111@columbia.edu>
2020-11-09 12:13:39 -0500

    responsive design

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,7 +3,7 @@ import { gsap } from 'gsap'
 
 const Signin = () => {
 
-    gsap.to('.card-wrapper', {y:200, duration:3});
+    gsap.fromTo('div .card', {y:100, opacity:0}, {y:0, opacity:1, stagger:0.1,});
 
     return (
         <>
@@ -18,8 +18,31 @@ const Signin = () => {
                 </nav>
 
                 <div className="card-wrapper">
-                    <h1>About Us</h1>
+                    <h1>The Squad</h1>
                 </div>
+
+                <div className="intro-wrapper">
+                <div className="card left z-depth-0 blue-grey lighten-5">
+                    <img className="intro-images" src="/images/Aliyah2.jpg" alt="" />
+                    <h3>Aliyah</h3>
+                    <p><strong>Aliyah Gamble</strong> is a junior developer based in the Atlanta area.</p>
+                </div>    
+                <div className="card z-depth-0 blue-grey lighten-5">
+                    <img className="intro-images" src="/images/shaiah_profile.jpg" alt="" />    
+                    <h3>Luke</h3>
+                    <p><strong>Luke Brazil</strong> is a junior developer based in the Houston area.</p>
+                 </div>
+                <div className="card z-depth-0 blue-grey lighten-5">
+                    <img className="intro-images" src="/images/shaiah_profile.jpg" alt="" /> 
+                    <h3>Shaiah</h3>
+                    <p><strong>Shaiah Emigh-doyle</strong> is a junior developer based in the NYC area.</p>
+                </div>
+                <div className="card right z-depth-0 blue-grey lighten-5">
+                    <img className="intro-images" src="/images/tait.JPG" alt="" />
+                    <h3>Tait</h3>
+                    <p><strong>Tait Loughridge</strong> is a junior developer based in the Houston area.</p>
+                </div>
+            </div>
 
         </>
     )
