3a0a82a841a1a66d3be7f892ef4a8b4ecbe69450
Luke Brazil <Luke.brazil13@icloud.com>
2020-11-11 11:31:22 -0600

    added jobs api

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,49 +1,54 @@
-import React, {useState, useEffect, useRef} from 'react'
-import { Route, Link } from 'react-router-dom';
-import M from 'materialize-css';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import { Route, Link } from "react-router-dom";
+import M from "materialize-css";
+import axios from "axios";
 
 //Componenets
-import News from './News';
-import YouTube from './YouTube/YouTube';
-import Jobs from './Jobs'
-import FlashcardList from './FlashcardList'
+import News from "./News";
+import YouTube from "./YouTube/YouTube";
+import Jobs from "./Jobs";
+import FlashcardList from "./FlashcardList";
+import JobsApi from './JobsApi';
 
 let instance = M.Tabs.init();
 
 const Dashboard = () => {
-    const [flashcards, setFlashcards] = useState([]);
-    const [categories, setCategories] = useState([]);
-  
-    const categoryEl = useRef();
-    const amountEl = useRef();
-  
-    useEffect(() => {
-      axios.get("https://opentdb.com/api_category.php").then((res) => {
-        setCategories(res.data.trivia_categories);
-      });
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api_category.php").then((res) => {
+      setCategories(res.data.trivia_categories);
     });
-  
-    useEffect(() => {
-     axios.get('https://secret-gorge-43213.herokuapp.com/questions').then((res) => {
-       console.log('API data >>>>', res.data)
-     })
-    }, []);
-  
-    function decodeString(str) {
-      const textArea = document.createElement("textarea");
-      textArea.innerHTML = str;
-      return textArea.value;
-    }
-  
-    function handleSubmit(e) {
-      e.preventDefault()
-      axios.get("https://opentdb.com/api.php", {
+  });
+
+  useEffect(() => {
+    axios
+      .get("https://secret-gorge-43213.herokuapp.com/questions")
+      .then((res) => {
+        console.log("API data >>>>", res.data);
+      });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get("https://opentdb.com/api.php", {
         params: {
           amount: amountEl.current.value,
-          category: categoryEl.current.value
-        }
-      }).then((res) => {
+          category: categoryEl.current.value,
+        },
+      })
+      .then((res) => {
         setFlashcards(
           res.data.results.map((questionItem, index) => {
             const answer = decodeString(questionItem.correct_answer);
@@ -60,111 +65,159 @@ const Dashboard = () => {
           })
         );
       });
-    }
-  
-    async function javascriptButton(e) {
-      e.preventDefault()
-      await axios.get('https://secret-gorge-43213.herokuapp.com/questions/javascript').then((res) => {
+  }
+
+  async function javascriptButton(e) {
+    e.preventDefault();
+    await axios
+      .get("https://secret-gorge-43213.herokuapp.com/questions/javascript")
+      .then((res) => {
         setFlashcards(
           res.data.rows.map((questionsItem, index) => {
             return {
               id: questionsItem.id,
               question: questionsItem.question,
-              answer: questionsItem.answer
-            }
+              answer: questionsItem.answer,
+            };
           })
         );
-  
-  
-      })
-  
-    }
-  
-    async function reactButton(e) {
-      e.preventDefault()
-      await axios.get('https://secret-gorge-43213.herokuapp.com/questions/react').then((res) => {
+      });
+  }
+
+  async function reactButton(e) {
+    e.preventDefault();
+    await axios
+      .get("https://secret-gorge-43213.herokuapp.com/questions/react")
+      .then((res) => {
         setFlashcards(
           res.data.rows.map((questionsItem, index) => {
             return {
               id: questionsItem.id,
               question: questionsItem.question,
-              answer: questionsItem.answer
-            }
+              answer: questionsItem.answer,
+            };
           })
         );
-  
-  
-      })
-  
-    }
-  
-    async function pythonButton(e) {
-      e.preventDefault()
-      await axios.get('https://secret-gorge-43213.herokuapp.com/questions/python').then((res) => {
+      });
+  }
+
+  async function pythonButton(e) {
+    e.preventDefault();
+    await axios
+      .get("https://secret-gorge-43213.herokuapp.com/questions/python")
+      .then((res) => {
         setFlashcards(
           res.data.rows.map((questionsItem, index) => {
             return {
               id: questionsItem.id,
               question: questionsItem.question,
-              answer: questionsItem.answer
-            }
+              answer: questionsItem.answer,
+            };
           })
         );
-  
-  
-      })
-  
-    }
-    return (
-        <>
-            <nav>
-                <div className="nav-wrapper z-depth-0 blue-grey darken-3">
-                    <a href="#" className="brand-logo center">j.DevSpace</a>
-                    <ul id="nav-mobile" className="right">
-                        <li><a href="/aboutus" className="waves-effect waves-light btn-small light-blue darken-2">About Us</a></li>
-                        <li><a href="/" className="waves-effect waves-light btn-small light-blue darken-2">Home</a></li>
+      });
+  }
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper z-depth-0 blue-grey darken-3">
+          <a href="#" className="brand-logo center">
+            j.DevSpace
+          </a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a
+                href="/aboutus"
+                className="waves-effect waves-light btn-small light-blue darken-2"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="waves-effect waves-light btn-small light-blue darken-2"
+              >
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-                    </ul>
-                </div>
-            </nav>
+      <div className="col s12">
+        <ul className="tabs dashboard-links blue-grey darken-3">
+          <li className="tab col s3">
+            <Link className="blue-grey-text text-lighten-5" to="/dashboard">
+              News
+            </Link>
+          </li>
+          <li className="tab col s3">
+            <Link
+              className="blue-grey-text text-lighten-5"
+              to="/dashboard/flashcards"
+            >
+              Flashcards
+            </Link>
+          </li>
+          <li className="tab col s3">
+            <Link
+              className="blue-grey-text text-lighten-5"
+              to="/dashboard/video"
+            >
+              Videos
+            </Link>
+          </li>
+          <li className="tab col s3">
+            <Link
+              className="blue-grey-text text-lighten-5"
+              to="/dashboard/jobs"
+            >
+              Jobs
+            </Link>
+          </li>
+          <li className="tab col s3">
+            <Link className="blue-grey-text text-lighten-5" to="/dashboard/jobApi">
+              Jobs Api
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-                <div className='col s12'>
-                    <ul className="tabs dashboard-links blue-grey darken-3">
-                        <li className="tab col s3"><Link className='blue-grey-text text-lighten-5' to="/dashboard">News</Link></li>
-                        <li className="tab col s3"><Link className='blue-grey-text text-lighten-5' to="/dashboard/flashcards">Flashcards</Link></li>
-                        <li className="tab col s3"><Link className='blue-grey-text text-lighten-5' to="/dashboard/video">Videos</Link></li>
-                        <li className="tab col s3"><Link className='blue-grey-text text-lighten-5' to="/dashboard/jobs">Jobs</Link></li>
-                    </ul>
-                </div>
+      <Route exact path="/dashboard">
+        <News />
+      </Route>
 
-            <Route exact path="/dashboard">
-                <News />
-            </Route>
-            
-            <Route path="/dashboard/flashcards">
-                <div className='col s12'>
-                    <ul className="tabs dashboard-links blue-grey darken-3" onSubmit={handleSubmit}>
-                        <li className='tab col s3 blue-grey-text text-lighten-5' onClick={javascriptButton}>Javascript</li>
-                        <li className='tab col s3 blue-grey-text text-lighten-5' onClick={reactButton}>React</li>
-                        <li className='tab col s3 blue-grey-text text-lighten-5' onClick={pythonButton}>Python</li>
-                    </ul>
-                </div>
-                <div className="container">
-                    <FlashcardList flashcards={flashcards} />
-                </div>
-            </Route>
+      <Route path="/dashboard/flashcards">
+        <form className="header" onSubmit={handleSubmit}>
+          <button className="button" onClick={javascriptButton}>
+            Javascript
+          </button>
+          <button className="button" onClick={reactButton}>
+            React
+          </button>
+          <button className="button" onClick={pythonButton}>
+            Python
+          </button>
+        </form>
+        <div className="container">
+          <FlashcardList flashcards={flashcards} />
+        </div>
+        <FlashcardList flashcards={flashcards} />
+      </Route>
 
-            <Route path="/dashboard/video">
-                <YouTube />
-            </Route>
+      <Route path="/dashboard/video">
+        <YouTube />
+      </Route>
 
-            <Route path="/dashboard/jobs">
-                <Jobs />
-            </Route>
-
-
-        </>
-    )
-}
+      <Route path="/dashboard/jobs">
+        <Jobs />
+      </Route>
+      <Route path="/dashboard/jobApi">
+        <JobsApi />
+      </Route>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,97 +1,70 @@
-<<<<<<< HEAD
-import { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Component, useState, useEffect } from "react";
+import { Route } from "react-router-dom";
 
 const authKey = "a/tkHYSdRszVgOLaiZYgZ3AxA3TIw6480RxWxWb0O0o=";
 const host = "data.usajobs.gov";
 const userAgent = "agamble9@student.gsu.edu";
 
 class Jobs extends Component {
-    state = {
-        jobSearch: "",
-        jobName: [],
-    };
+  state = {
+    jobSearch: "",
+    jobName: [],
+  };
 
-    _handleChange = (newText) => {
-        this.setState({
-            jobSearch: newText,
-        });
-    };
+  _handleChange = (newText) => {
+    this.setState({
+      jobSearch: newText,
+    });
+  };
 
-    _handleSearch = async () => {
-        // const searchData = await this.loadData();
-        // this.setState({
-        //     jobName: [...this.state.jobName, searchData]
-        // });
-        this.getJobs(this.state.jobSearch)
-    };
+  _handleSearch = async () => {
+    // const searchData = await this.loadData();
+    // this.setState({
+    //     jobName: [...this.state.jobName, searchData]
+    // });
+    this.getJobs(this.state.jobSearch);
+  };
 
-
-
-getJobs = async (jobSearch) => {
-  const data = await fetch(`https://data.usajobs.gov/api/search?Keyword=${jobSearch}`, {
-    method: "GET",
-    headers: {
-      Host: host,
-      "User-Agent": userAgent,
-      "Authorization-Key": authKey
-    }
-  }).then((response) => response.json());
-  console.log(data.SearchResult.SearchResultItems);
-};
-    // loadData = async () => {
-
-    //     const response = await fetch(
-    //         `https://data.usajobs.gov/api/Search?Keyword=Software&apiKey=a/tkHYSdRszVgOLaiZYgZ3AxA3TIw6480RxWxWb0O0o=`);
-    //    const data = await response.json();
-    //     return data;
-    // };
-    
+  getJobs = async (jobSearch) => {
+    const data = await fetch(
+      `https://data.usajobs.gov/api/search?Keyword=${jobSearch}`,
+      {
+        method: "GET",
+        headers: {
+          Host: host,
+          "User-Agent": userAgent,
+          "Authorization-Key": authKey,
+        },
+      }
+    ).then((response) => response.json());
+    console.log(data.SearchResult.SearchResultItems);
+  };
 
 
-
-
-
-
-    render() {
-        return (
-            <>
-                <Route>
-                    <h1>Jobs</h1>
-                </Route>
-                <form>
-                    <lable>
-                        Loading job data...
-                 <input type="text"
-                onChange={(event) => this._handleChange(event.target.value)} />
-                </lable>
-                </form>
-                <button type="button" onClick={this._handleSearch}> 
-                    Search Jobs
-                </button>
-                {this.state.jobName.map(article => {
-                 return ( <li>{Jobs}</li>
-                 )}
-                )}
-            </>
-            )
-        
-    };
-  
-};
-
-
- 
-=======
-import React from 'react'
-
-const Jobs = () => {
+  render() {
     return (
-        <>
-            <h1>Jobs</h1>
-        </>
-    )
+      <>
+        <Route>
+          <h1>Jobs</h1>
+        </Route>
+        <form>
+          <lable>
+            Loading job data...
+            <input
+              type="text"
+              onChange={(event) => this._handleChange(event.target.value)}
+            />
+          </lable>
+        </form>
+        <button type="button" onClick={this._handleSearch}>
+          Search Jobs
+        </button>
+        {this.state.jobName.map((article) => {
+          return <li>{Jobs}</li>;
+        })}
+      </>
+    );
+  }
 }
->>>>>>> 6049806b4c4fa81850dd8107631d5dd3f99bc3a8
 
 export default Jobs;
