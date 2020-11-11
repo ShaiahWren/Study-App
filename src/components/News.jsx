cb5cfb7425f6916202c22afdd9e610ec7ee0c2e3
Shaiah Wren <sde2111@columbia.edu>
2020-11-11 14:55:37 -0500

    styling news feed

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { gsap } from 'gsap'
+import { Row, Col, Card, CardTitle } from 'react-materialize';
 
 class News extends Component {
     state = {
         newsLoads: [],
     };
-     
+
     async componentDidMount() {
         const newsData = await this.loadData();
         this.setState({
@@ -13,47 +14,58 @@ class News extends Component {
         });
     };
 
-     _handleNews = async () => {
+    _handleNews = async () => {
         const newsData = await this.loadData();
         this.setState({
             newsLoads: newsData.articles
         });
-     };
+    };
 
-     loadData = async () => {
-         const response = await fetch(
-             `http://newsapi.org/v2/everything?q=programming&apiKey=fd362930e3b74a278a7cff2c0cf8b3f9`);
+    loadData = async () => {
+        const response = await fetch(
+            `http://newsapi.org/v2/everything?q=programming&apiKey=fd362930e3b74a278a7cff2c0cf8b3f9`);
         const data = await response.json();
-         return data;
-     };
+        return data;
+    };
 
-     render() {
+    render() {
 
         // gsap.fromTo('div .newsCard', {opacity:0}, {opacity:1, stagger:0.3, duration:3});
 
         return (
             <>
-             <h1> News </h1>
-             {/* <form>
-                <button type="button" onClick={() => this._handleNews()}> 
-                News Update
-                </button>
-             </form> */}
-             {this.state.newsLoads.map(article => {
-                 return ( 
-                    <div className='newsCard card z-depth-0'>
-                        <div className='newsCol1'>
-                            <a href={article.url}><img className='newsImg' src={article.urlToImage}></img></a>
+                <h1> News </h1>
+
+                {this.state.newsLoads.map(article => {
+                    return (
+                        <div className="news-wrapper">
+                        <div className="row">
+                            <div class="col s12 m7">
+                                <div className="card center">
+                                    <div class="card-image">
+                                        <img src={article.urlToImage} />
+                                    </div>
+                                    <div class="card-content">
+                                        <h2 className="card-title" href={article.url} target="_blank">{article.title}</h2>
+
+                                        <p>{article.description}</p>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
-                        <div className='newsCol2'>
-                            <a href={article.url}><h3>{article.title}</h3></a>
-                            <a href={article.url}><p>{article.description}</p></a>
                         </div>
-                    </div>)})
-            };
-    
-            </> 
-        )}
+
+
+
+
+                    )
+                })
+                };
+
+            </>
+        )
+    }
 
 };
 
