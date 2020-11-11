f691a6683c47fb172b86c431ba840603fc9869aa
Tait Loughridge <68408313+TaitLoughridge@users.noreply.github.com>
2020-11-11 14:35:13 -0600

    css for jobs page

--- src/components/JobsApi.jsx
+++ src/components/JobsApi.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
-import axios from "axios";
+import axios from 'axios';
 
 // loadData = async () => {
 //     const response = await fetch(
@@ -12,54 +12,65 @@ import axios from "axios";
 //   };
 
 export default function JobsApi() {
-  const [jobsData, setJobsData] = useState([]);
+	const [ jobsData, setJobsData ] = useState([]);
 
-const getJavascriptJobs = () => {
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=javascript&location=us",
-        { headers: { "Access-Control-Allow-Origin": "*" } }
-      )
-      .then(function (response) {
-        setJobsData(response.data)
-      });
-}
-const getReactJobs = () => {
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=react&location=us",
-        { headers: { "Access-Control-Allow-Origin": "*" } }
-      )
-      .then(function (response) {
-        setJobsData(response.data)
-      });
-}
-const getPythonJobs = () => {
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=python&location=us",
-        { headers: { "Access-Control-Allow-Origin": "*" } }
-      )
-      .then(function (response) {
-        setJobsData(response.data)
-      });
-}
+	const getJavascriptJobs = () => {
+		axios
+			.get(
+				'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=javascript&location=us',
+				{ headers: { 'Access-Control-Allow-Origin': '*' } }
+			)
+			.then(function(response) {
+				setJobsData(response.data);
+			});
+	};
+	const getReactJobs = () => {
+		axios
+			.get('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=react&location=us', {
+				headers: { 'Access-Control-Allow-Origin': '*' }
+			})
+			.then(function(response) {
+				setJobsData(response.data);
+			});
+	};
+	const getPythonJobs = () => {
+		axios
+			.get(
+				'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=python&location=us',
+				{ headers: { 'Access-Control-Allow-Origin': '*' } }
+			)
+			.then(function(response) {
+				setJobsData(response.data);
+			});
+	};
 
-  return (
-    <div>
-    <button onClick={getJavascriptJobs}>JavaScript Jobs</button>
-    <button onClick={getReactJobs}>React Jobs</button>
-    <button onClick={getPythonJobs}>Python Jobs</button>
-      { jobsData.map((job) => {
-                return <div>
-                    <p>{job.title}</p>
-                    <p>{job.location}</p>
-                    <p>{job.type}</p>
-                    <p>{job.comapny}</p>
-                    <a href={job.company_url} target='_blank'>Link to Job</a>
-                    
-                </div>
-            })}
-    </div>
-  );
+	return (
+		<div>
+			<button className="btn-small teal darken-1" onClick={getJavascriptJobs}>
+				JavaScript Jobs
+			</button>
+			<button className="btn-small teal darken-1" onClick={getReactJobs}>
+				React Jobs
+			</button>
+			<button className="btn-small teal darken-1" onClick={getPythonJobs}>
+				Python Jobs
+			</button>
+			{jobsData.map((job) => {
+				return (
+					<div className="jobsCard card z-depth-0">
+						<h5>{job.title}</h5>
+						<p>
+							<strong>{job.company}</strong>
+						</p>
+						<p>
+							{job.location} - {job.type}
+						</p>
+						<a href={job.company_url} target="_blank">
+							<button className="btn-small teal darken-1 jobBtn">Link to Job</button>
+						</a>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
